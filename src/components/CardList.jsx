5fc907e5ab5e79d2ee23ce325bb187377320390e
Cristian-Sosa <64815792+Cristian-Sosa@users.noreply.github.com>
2022-07-04 16:59:06 -0300

    feature: navbar menu

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,31 +1,35 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
+import PagesBar from "./PagesBar";
 
 function CardList() {
   const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${page}`
+      );
       const data = await response.json();
       setCharacters(data.results);
     }
 
     fetchData();
-  }, []);
+  }, [page]);
 
   return (
-    <div className="bg-dark text-white">
-      <h1 className="display text-center py-5">Rick and Morty API</h1>
-      <div className="container">
+    <main className="bg-dark text-white pb-5">
+      <h1 className="display-1 text-center py-5">Rick and Morty API</h1>
+      <section className="container">
         <div className="row">
           {characters.map((character) => {
             return <Card key={character.id} character={character} />;
           })}
         </div>
-      </div>
-    </div>
+      </section>
+      <PagesBar page={page} setPage={setPage} />
+    </main>
   );
 }
-
 export default CardList;
