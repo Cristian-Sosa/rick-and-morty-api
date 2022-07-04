ad8e1d73751733aa5ad68d85468099c692493f18
Cristian-Sosa <64815792+Cristian-Sosa@users.noreply.github.com>
2022-07-04 15:57:36 -0300

    feature: create button view more and corrections in styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,23 @@
 function Card({ character }) {
   return (
-    <div className="bg-dark col-md-4 d-flex flex-column justify-content-center">
+    <div className="bg-dark col-md-4 d-flex flex-column justify-content-center h-100 py-5 px-5 my-5">
       <img
-        className="rounded-pill m-auto"
+        className="rounded-pill m-auto mb-4 w-50 border border-5 border-secondary"
         src={character.image}
         alt={character.name}
       />
-      <h3 className="text-center py-4">{character.name}</h3>
+      <h3 className="text-center mb-2">{character.name}</h3>
+      <span className="text-center text-white-50">
+        Status: {character.status}
+      </span>
+      <span className="text-center text-white-50">
+        Species: {character.species}
+      </span>
+      <span className="text-center text-white-50">
+        Gender: {character.gender}
+      </span>
+
+      <button type='button' className="mt-5 btn btn-primary">View More</button>
     </div>
   );
 }
